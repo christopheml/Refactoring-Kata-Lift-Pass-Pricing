8a3bea26b7ba3a164569d62af89795d90fe3d36a
Christophe MICHEL <christophe_michel@ext.connect-tech.sncf>
2023-03-30 15:03:44 +0200

    Test harness for getting prices

--- java/src/test/java/dojo/liftpasspricing/PricesTest.java
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -27,21 +27,74 @@ public class PricesTest {
     }
 
     @Test
-    public void doesSomething() {
-        JsonPath response = RestAssured.
-            given().
-                port(4567).
-            when().
-                // construct some proper url parameters
-                get("/prices").
-            then().
-                assertThat().
-                    statusCode(200).
-                assertThat().
-                    contentType("application/json").
-            extract().jsonPath();
+    public void price_1day() {
+        assertPriceEquals(35, "type=1jour");
+    }
 
-        assertEquals("putSomehtingHere", response.get("putSomehtingHere"));
+    @Test
+    void price_1day_regular_day() {
+        assertPriceEquals(35, "type=1jour&date=2023-03-22");
+    }
+
+    @Test
+    public void price_1day_kid() {
+        assertPriceEquals(0, "type=1jour&age=4");
+    }
+
+    @Test
+    public void price_1day_teen() {
+        assertPriceEquals(25, "type=1jour&age=14");
+    }
+
+    @Test
+    void price_1day_monday() {
+        assertPriceEquals(23, "type=1jour&date=2023-03-20");
+    }
+
+    @Test
+    void price_1day_monday_senior() {
+        assertPriceEquals(18, "type=1jour&date=2023-03-20&age=73");
+    }
+
+    @Test
+    void price_1day_monday_adult() {
+        assertPriceEquals(23, "type=1jour&date=2023-03-20&age=25");
+    }
+
+    @Test
+    void price_1day_monday_holiday() {
+        assertPriceEquals(35, "type=1jour&date=2019-02-18");
+    }
+
+    @Test
+    public void price_night_kid() {
+        assertPriceEquals(0, "type=night&age=5");
+    }
+
+    @Test
+    void price_night_adult() {
+        assertPriceEquals(19, "type=night&age=25");
+    }
+
+    @Test
+    void price_night_senior() {
+        assertPriceEquals(8, "type=night&age=72");
+    }
+
+    private void assertPriceEquals(int cost, String queryParams) {
+        JsonPath response = RestAssured
+                .given()
+                .port(4567)
+                .when()
+                .get("/prices?" + queryParams)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .assertThat()
+                .contentType("application/json")
+                .extract().jsonPath();
+
+        assertEquals(cost, response.getInt("cost"));
     }
 
 }
